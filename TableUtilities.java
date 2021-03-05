b047a59c9306b7ecbaa0d95a6022cdf4f6e6ae29
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-05 18:05:34 -0500

    Filled out TableUtilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,56 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String returnString = "";
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5; j++){
+                if((i*j) > 99) {
+                    returnString = returnString + (i * j) + " |";
+                }
+                else if((i*j) > 9){
+                    returnString = returnString + " " + (i * j) + " |";
+                } else {
+                    returnString = returnString + "  " + (i * j) + " |";
+                }
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String returnString = "";
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                if((i*j) > 99) {
+                    returnString = returnString + (i * j) + " |";
+                }
+                else if((i*j) > 9){
+                    returnString = returnString + " " + (i * j) + " |";
+                } else {
+                    returnString = returnString + "  " + (i * j) + " |";
+                }
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+                String returnString = "";
+                for(int i = 1; i <= tableSize; i++){
+                    for(int j = 1; j <= tableSize; j++){
+                        if((i*j) > 99) {
+                            returnString = returnString + (i * j) + " |";
+                        }
+                        else if((i*j) > 9){
+                            returnString = returnString + " " + (i * j) + " |";
+                        } else {
+                            returnString = returnString + "  " + (i * j) + " |";
+                        }
+                    }
+                    returnString = returnString + "\n";
+                }
+                return returnString;
     }
 }
