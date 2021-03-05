c5947fea324c0afeb270152934eec1d1cb993bf2
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-05 18:12:11 -0500

    Fileld out TriangleUtilities

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,44 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String returnString = "";
+        for(int i = 0; i < numberOfStars; i++){
+            returnString = returnString + "*";
+        }
+        return returnString;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String returnString = "";
+        for(int i = 1; i <= numberOfRows; i++){
+            for(int j = 1; j <= i; j++){
+                returnString = returnString + "*";
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String returnString = "";
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                returnString = returnString + "*";
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String returnString = "";
+        for(int i = 1; i <= 9; i++){
+            for(int j = 1; j <= i; j++){
+                returnString = returnString + "*";
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
     }
 }
