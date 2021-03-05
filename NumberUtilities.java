7f078b741107cc47ee4f9b67e3b2f93ceeff9a68
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-05 17:53:36 -0500

    Filled out NumberUtilities

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,72 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String returnString = "";
+
+        for(int i = 0; i < stop; i++){
+            returnString = returnString + i;
+        }
+
+        return returnString;
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String returnString = "";
+
+        for(int i = start; i < stop; i++){
+            returnString = returnString + i;
+        }
+
+        return returnString;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String returnString = "";
+
+        for(int i = start; i < stop; i = i + step){
+            returnString = returnString + i;
+        }
+
+        return returnString;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String returnString = "";
+
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 0) {
+                returnString = returnString + i;
+            }
+        }
+
+        return returnString;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String returnString = "";
+
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 1) {
+                returnString = returnString + i;
+            }
+        }
+
+        return returnString;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String returnString = "";
+        int toPower;
+
+        for(int i = start; i <= stop; i++){
+            toPower = (int) Math.pow(i, exponent);
+            returnString = returnString + toPower;
+        }
+
+        return returnString;
     }
 }
